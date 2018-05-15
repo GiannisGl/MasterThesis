646002464380a383998caf7e76b0acef1d5063d1
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-05-15 21:49:55 +0300

    changed loss as sum of featsLoss and distLoss with a lamda factor

--- LearnDistance/losses.py
+++ LearnDistance/losses.py
@@ -10,6 +10,7 @@ class distance_loss(torch.nn.Module):
     def forward(self, delta, input1, input2, input3, featsModel, distanceModel):
         delta = torch.ones(input1.size()[0])*delta
         zero = torch.zeros(input1.size()[0])
+        lamda = 1
         if torch.cuda.is_available():
             torch.set_default_tensor_type('torch.cuda.FloatTensor')
         
@@ -32,30 +33,32 @@ class distance_loss(torch.nn.Module):
         learnedDist32 = distanceModel(input3, input2)
 
         # terms that preserve distance
-        loss = mseLoss(dist12, learnedDist12)
-        loss += mseLoss(dist13, learnedDist13)
-        loss += mseLoss(dist23, learnedDist23)
+        featsLoss = mseLoss(dist12, learnedDist12)
+        featsLoss += mseLoss(dist13, learnedDist13)
+        featsLoss += mseLoss(dist23, learnedDist23)
 
         # terms that enforce symmetry
-        loss += mseLoss(learnedDist12, learnedDist21)
-        loss += mseLoss(learnedDist13, learnedDist31)
-        loss += mseLoss(learnedDist23, learnedDist32)
+        distLoss = mseLoss(learnedDist12, learnedDist21)
+        distLoss += mseLoss(learnedDist13, learnedDist31)
+        distLoss += mseLoss(learnedDist23, learnedDist32)
 
         # terms that enforce distance greater than delta
-        loss += mseLoss(relu(delta - learnedDist12), zero)
-        loss += mseLoss(relu(delta - learnedDist13), zero)
-        loss += mseLoss(relu(delta - learnedDist23), zero)
-        loss += mseLoss(relu(delta - learnedDist21), zero)
-        loss += mseLoss(relu(delta - learnedDist32), zero)
-        loss += mseLoss(relu(delta - learnedDist31), zero)
+        distLoss += mseLoss(relu(delta - learnedDist12), zero)
+        distLoss += mseLoss(relu(delta - learnedDist13), zero)
+        distLoss += mseLoss(relu(delta - learnedDist23), zero)
+        distLoss += mseLoss(relu(delta - learnedDist21), zero)
+        distLoss += mseLoss(relu(delta - learnedDist32), zero)
+        distLoss += mseLoss(relu(delta - learnedDist31), zero)
 
         # terms that enforce triangular inequality
-        loss += mseLoss(relu(learnedDist13 - learnedDist12 - learnedDist23), zero)
-        loss += mseLoss(relu(learnedDist31 - learnedDist32 - learnedDist21), zero)
-        loss += mseLoss(relu(learnedDist23 - learnedDist21 - learnedDist13), zero)
-        loss += mseLoss(relu(learnedDist32 - learnedDist31 - learnedDist12), zero)
-        loss += mseLoss(relu(learnedDist12 - learnedDist13 - learnedDist32), zero)
-        loss += mseLoss(relu(learnedDist21 - learnedDist23 - learnedDist31), zero)
+        distLoss += mseLoss(relu(learnedDist13 - learnedDist12 - learnedDist23), zero)
+        distLoss += mseLoss(relu(learnedDist31 - learnedDist32 - learnedDist21), zero)
+        distLoss += mseLoss(relu(learnedDist23 - learnedDist21 - learnedDist13), zero)
+        distLoss += mseLoss(relu(learnedDist32 - learnedDist31 - learnedDist12), zero)
+        distLoss += mseLoss(relu(learnedDist12 - learnedDist13 - learnedDist32), zero)
+        distLoss += mseLoss(relu(learnedDist21 - learnedDist23 - learnedDist31), zero)
+
+        loss = featsLoss+lamda*distLoss
 
         return loss
 
