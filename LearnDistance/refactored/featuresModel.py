95caf41ffa1ce9a50e8266ef8efb21a9667ef663
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-07-05 10:13:42 +0200

    added loss fixFeatsConv, fixed lenetFix

--- LearnDistance/refactored/featuresModel.py
+++ LearnDistance/refactored/featuresModel.py
@@ -138,6 +138,48 @@ class FeatsLeNet5Full(nn.Module):
         return output
 
 
+
+class FeatsLeNet5Fix(nn.Module):
+
+    # Input - 1x28x28
+    # C1 - 6@24x24 (5x5 kernel)
+    # tanh
+    # S2 - 6@12x12 (2x2 kernel, stride 2) Subsampling
+    # C3 - 16@8x8 (5x5 kernel, complicated shit)
+    # tanh
+    # S4 - 16@4x4 (2x2 kernel, stride 2) Subsampling
+    # C5 - 120@1x1 (5x5 kernel)
+    # F6 - 84
+    # tanh
+    # F7 - 10 (Output)
+
+    def __init__(self):
+        super(FeatsLeNet5Fix, self).__init__()
+
+        self.convnet = nn.Sequential(OrderedDict([
+            ('c1', nn.Conv2d(1, 6, kernel_size=(5, 5))),
+            ('relu1', nn.ReLU()),
+            ('s2', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c3', nn.Conv2d(6, 16, kernel_size=(5, 5))),
+            ('relu3', nn.ReLU()),
+            ('s4', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c5', nn.Conv2d(16, 120, kernel_size=(4, 4))),
+            ('relu5', nn.ReLU())
+        ]))
+
+        self.fc = nn.Sequential(OrderedDict([
+            ('f6', nn.Linear(120, 84)),
+            ('relu6', nn.ReLU()),
+            ('f7', nn.Linear(84, 10))
+        ]))
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(-1, 120)
+        output = self.fc(output)
+        return output
+
+
 class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
@@ -192,6 +234,19 @@ def featsLenet(pretrained=False, **kwargs):
 
 def featsLenetFull(pretrained=False, **kwargs):
     model = FeatsLeNet5Full(**kwargs)
+    if pretrained:
+        modelFilename = '../../trainModels/models/modellenet5mnist_Iter6.torchmodel'
+        pretrained = torch.load(modelFilename)
+        pretrained_dict = pretrained.state_dict()
+        model_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+
+def featsLenetFix(pretrained=False, **kwargs):
+    model = FeatsLeNet5Fix(**kwargs)
     if pretrained:
         modelFilename = '../../trainModels/models/modellenet5mnist_Iter6.torchmodel'
         pretrained = torch.load(modelFilename)
--- LearnDistance/refactored/losses.py
+++ LearnDistance/refactored/losses.py
@@ -105,6 +105,212 @@ class distance_loss(torch.nn.Module):
         return loss
 
 
+class distance_loss_fixFeats(torch.nn.Module):
+    def __init__(self, writer, delta, lamda):
+        super(distance_loss, self).__init__()
+        self.writer = writer
+        self.step = 0
+        self.delta = delta
+        self.lamda = lamda
+
+    def forward(self, input1, input2, input3, featsModel, distanceModel):
+        self.step += 1
+        delta = torch.ones(input1.size()[0]) * self.delta
+        zero = torch.zeros(input1.size()[0])
+        if torch.cuda.is_available():
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+        # get features of inputs
+        input1feats = featsModel.forward(input1)
+        input1augm = random_augmentation(input1)
+        input1augmfeats = featsModel.forward(input1augm)
+        input2feats = featsModel.forward(input2)
+        input2augm = random_augmentation(input2)
+        input2augmfeats = featsModel.forward(input2augm)
+        input3feats = featsModel.forward(input3)
+        input3augm = random_augmentation(input3)
+        input3augmfeats = featsModel.forward(input3augm)
+
+        # get L2 distance of the 3 pairs of features
+        dist12 = mse_batch_loss(input1feats, input2feats)
+        dist13 = mse_batch_loss(input1feats, input3feats)
+        dist23 = mse_batch_loss(input2feats, input3feats)
+
+        # get learned distance of the 3 pairs both ways
+        learnedDist12 = distanceModel(input1, input2)
+        learnedDist21 = distanceModel(input2, input1)
+        learnedDist13 = distanceModel(input1, input3)
+        learnedDist31 = distanceModel(input3, input1)
+        learnedDist23 = distanceModel(input2, input3)
+        learnedDist32 = distanceModel(input3, input2)
+        learnedDist11 = distanceModel(input1, input1)
+        learnedDist22 = distanceModel(input2, input2)
+        learnedDist33 = distanceModel(input3, input3)
+
+        # Features model terms
+        featsLoss = 0
+        # terms that preserve distance
+        featsLossDist = mseLoss(dist12, learnedDist12)
+        featsLossDist += mseLoss(dist13, learnedDist13)
+        featsLossDist += mseLoss(dist23, learnedDist23)
+        self.writer.add_scalar(tag='featsLossDist', scalar_value=featsLossDist, global_step=self.step)
+        featsLoss += featsLossDist
+        # terms that enforce clustering
+        featsLossClust = mseLoss(input1feats, input1augmfeats)
+        featsLossClust += mseLoss(input2feats, input2augmfeats)
+        featsLossClust += mseLoss(input3feats, input3augmfeats)
+        self.writer.add_scalar(tag='featsLossClust', scalar_value=featsLossClust, global_step=self.step)
+        featsLoss += featsLossClust
+
+        self.writer.add_scalar(tag='featsLoss', scalar_value=featsLoss, global_step=self.step)
+
+        # Distance model terms
+        distLoss = 0
+        # terms that enforce 0 distance for same inputs
+        distLossId = mseLoss(learnedDist11, zero)
+        distLossId += mseLoss(learnedDist22, zero)
+        distLossId += mseLoss(learnedDist33, zero)
+        self.writer.add_scalar(tag='distLossId', scalar_value=distLossId, global_step=self.step)
+        distLoss += distLossId
+
+        # terms that enforce symmetry
+        distLossSymm = mseLoss(learnedDist12, learnedDist21)
+        distLossSymm += mseLoss(learnedDist13, learnedDist31)
+        distLossSymm += mseLoss(learnedDist23, learnedDist32)
+        self.writer.add_scalar(tag='distLossSymm', scalar_value=distLossSymm, global_step=self.step)
+        distLoss += distLossSymm
+
+        # terms that enforce distance greater than delta
+        distLossDelta = mseLoss(relu(delta - learnedDist12), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist13), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist23), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist21), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist32), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist31), zero)
+        self.writer.add_scalar(tag='distLossDelta', scalar_value=distLossDelta, global_step=self.step)
+        distLoss += distLossDelta
+
+        # terms that enforce triangular inequality
+        distLossIneq = mseLoss(relu(learnedDist13 - learnedDist12 - learnedDist23), zero)
+        distLossIneq += mseLoss(relu(learnedDist31 - learnedDist32 - learnedDist21), zero)
+        distLossIneq += mseLoss(relu(learnedDist23 - learnedDist21 - learnedDist13), zero)
+        distLossIneq += mseLoss(relu(learnedDist32 - learnedDist31 - learnedDist12), zero)
+        distLossIneq += mseLoss(relu(learnedDist12 - learnedDist13 - learnedDist32), zero)
+        distLossIneq += mseLoss(relu(learnedDist21 - learnedDist23 - learnedDist31), zero)
+        self.writer.add_scalar(tag='distLossIneq', scalar_value=distLossIneq, global_step=self.step)
+        distLoss += distLossIneq
+
+        self.writer.add_scalar(tag='distLoss', scalar_value=distLoss, global_step=self.step)
+
+        loss = featsLoss + self.lamda * distLoss
+        self.writer.add_scalar(tag='loss', scalar_value=loss, global_step=self.step)
+
+        return loss
+
+
+class distance_loss_fixFeatsConv(torch.nn.Module):
+    def __init__(self, writer, delta, lamda):
+        super(distance_loss, self).__init__()
+        self.writer = writer
+        self.step = 0
+        self.delta = delta
+        self.lamda = lamda
+
+    def forward(self, input1, input2, input3, featsModel, distanceModel):
+        self.step += 1
+        delta = torch.ones(input1.size()[0]) * self.delta
+        zero = torch.zeros(input1.size()[0])
+        if torch.cuda.is_available():
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+        # get features of inputs
+        input1feats = featsModel.convnet(input1)
+        input1augm = random_augmentation(input1)
+        input1augmfeats = featsModel.convnet(input1augm)
+        input2feats = featsModel.convnet(input2)
+        input2augm = random_augmentation(input2)
+        input2augmfeats = featsModel.convnet(input2augm)
+        input3feats = featsModel.convnet(input3)
+        input3augm = random_augmentation(input3)
+        input3augmfeats = featsModel.convnet(input3augm)
+
+        # get L2 distance of the 3 pairs of features
+        dist12 = mse_batch_loss(input1feats, input2feats)
+        dist13 = mse_batch_loss(input1feats, input3feats)
+        dist23 = mse_batch_loss(input2feats, input3feats)
+
+        # get learned distance of the 3 pairs both ways
+        learnedDist12 = distanceModel(input1, input2)
+        learnedDist21 = distanceModel(input2, input1)
+        learnedDist13 = distanceModel(input1, input3)
+        learnedDist31 = distanceModel(input3, input1)
+        learnedDist23 = distanceModel(input2, input3)
+        learnedDist32 = distanceModel(input3, input2)
+        learnedDist11 = distanceModel(input1, input1)
+        learnedDist22 = distanceModel(input2, input2)
+        learnedDist33 = distanceModel(input3, input3)
+
+        # Features model terms
+        featsLoss = 0
+        # terms that preserve distance
+        featsLossDist = mseLoss(dist12, learnedDist12)
+        featsLossDist += mseLoss(dist13, learnedDist13)
+        featsLossDist += mseLoss(dist23, learnedDist23)
+        self.writer.add_scalar(tag='featsLossDist', scalar_value=featsLossDist, global_step=self.step)
+        featsLoss += featsLossDist
+        # terms that enforce clustering
+        featsLossClust = mseLoss(input1feats, input1augmfeats)
+        featsLossClust += mseLoss(input2feats, input2augmfeats)
+        featsLossClust += mseLoss(input3feats, input3augmfeats)
+        self.writer.add_scalar(tag='featsLossClust', scalar_value=featsLossClust, global_step=self.step)
+        featsLoss += featsLossClust
+
+        self.writer.add_scalar(tag='featsLoss', scalar_value=featsLoss, global_step=self.step)
+
+        # Distance model terms
+        distLoss = 0
+        # terms that enforce 0 distance for same inputs
+        distLossId = mseLoss(learnedDist11, zero)
+        distLossId += mseLoss(learnedDist22, zero)
+        distLossId += mseLoss(learnedDist33, zero)
+        self.writer.add_scalar(tag='distLossId', scalar_value=distLossId, global_step=self.step)
+        distLoss += distLossId
+
+        # terms that enforce symmetry
+        distLossSymm = mseLoss(learnedDist12, learnedDist21)
+        distLossSymm += mseLoss(learnedDist13, learnedDist31)
+        distLossSymm += mseLoss(learnedDist23, learnedDist32)
+        self.writer.add_scalar(tag='distLossSymm', scalar_value=distLossSymm, global_step=self.step)
+        distLoss += distLossSymm
+
+        # terms that enforce distance greater than delta
+        distLossDelta = mseLoss(relu(delta - learnedDist12), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist13), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist23), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist21), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist32), zero)
+        distLossDelta += mseLoss(relu(delta - learnedDist31), zero)
+        self.writer.add_scalar(tag='distLossDelta', scalar_value=distLossDelta, global_step=self.step)
+        distLoss += distLossDelta
+
+        # terms that enforce triangular inequality
+        distLossIneq = mseLoss(relu(learnedDist13 - learnedDist12 - learnedDist23), zero)
+        distLossIneq += mseLoss(relu(learnedDist31 - learnedDist32 - learnedDist21), zero)
+        distLossIneq += mseLoss(relu(learnedDist23 - learnedDist21 - learnedDist13), zero)
+        distLossIneq += mseLoss(relu(learnedDist32 - learnedDist31 - learnedDist12), zero)
+        distLossIneq += mseLoss(relu(learnedDist12 - learnedDist13 - learnedDist32), zero)
+        distLossIneq += mseLoss(relu(learnedDist21 - learnedDist23 - learnedDist31), zero)
+        self.writer.add_scalar(tag='distLossIneq', scalar_value=distLossIneq, global_step=self.step)
+        distLoss += distLossIneq
+
+        self.writer.add_scalar(tag='distLoss', scalar_value=distLoss, global_step=self.step)
+
+        loss = featsLoss + self.lamda * distLoss
+        self.writer.add_scalar(tag='loss', scalar_value=loss, global_step=self.step)
+
+        return loss
+
+
 class distance_AE_loss(torch.nn.Module):
     def __init__(self, writer, delta, lamda):
         super(distance_AE_loss, self).__init__()
