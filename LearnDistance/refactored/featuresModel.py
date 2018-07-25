ad82d42d1961f33393458f4cdd84eab20b0da446
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-07-25 19:45:36 +0200

    changed ReLU to ELU in featsModelfix

--- LearnDistance/refactored/featuresModel.py
+++ LearnDistance/refactored/featuresModel.py
@@ -152,18 +152,18 @@ class FeatsLeNet5Fix(nn.Module):
 
         self.convnet = nn.Sequential(OrderedDict([
             ('c1', nn.Conv2d(1, 6, kernel_size=(5, 5))),
-            ('relu1', nn.ReLU()),
+            ('relu1', nn.ELU()),
             ('s2', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
             ('c3', nn.Conv2d(6, 16, kernel_size=(5, 5))),
-            ('relu3', nn.ReLU()),
+            ('relu3', nn.ELU()),
             ('s4', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
             ('c5', nn.Conv2d(16, 120, kernel_size=(4, 4))),
-            ('relu5', nn.ReLU())
+            ('relu5', nn.ELU())
         ]))
 
         self.fc = nn.Sequential(OrderedDict([
             ('f6', nn.Linear(120, 84)),
-            ('relu6', nn.ReLU()),
+            ('relu6', nn.ELU()),
             ('f7', nn.Linear(84, 10))
         ]))
 
