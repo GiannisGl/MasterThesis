60ba6c7395a6ddc61d57d3b69183053fb5e54913
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-08-17 11:13:19 +0200

    changes

--- LearnDistance/refactored/inceptionModel.py
+++ LearnDistance/refactored/inceptionModel.py
@@ -121,7 +121,7 @@ class BasicConv2d(nn.Module):
 
     def __init__(self, in_channels, out_channels, **kwargs):
         super(BasicConv2d, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels,**kwargs)
         self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
 
     def forward(self, x):
