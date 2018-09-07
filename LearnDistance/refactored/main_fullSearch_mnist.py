6d7eeb95335dbfe65f5a318bb1657323dbb11350
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2018-09-07 18:41:27 +0200

    fixed bug with sorting in full search mnist

--- LearnDistance/refactored/main_fullSearch_mnist.py
+++ LearnDistance/refactored/main_fullSearch_mnist.py
@@ -21,13 +21,14 @@ if torch.cuda.is_available():
     N_train_batches = int(dataset_size/train_batch_size)
     datafolder = "/var/tmp/ioannis/data"
 else:
-    train_batch_size = 100
-    N_train_batches = int(dataset_size/train_batch_size)
+    train_batch_size = 10
+    N_train_batches = 2
+    N_test_samples = 1
     datafolder = "../../data"
 
 lamda = 1
 distPretrained = False
-modelname = "DistLeNetNoNorm%sAug%iOut%iDelta%iLamda%i" % (case, nAug, outDim, delta, lamda)
+#modelname = "DistLeNetNoNorm%sAug%iOut%iDelta%iLamda%i" % (case, nAug, outDim, delta, lamda)
 modelname = "DistLeNetNoNorm%sOut%iDelta%iLamda%i" % (case, outDim, delta, lamda)
 log_name = "fullSearch%s%s_Iter%i" % (dataset, modelname, trainstep)
 model_folder = "trainedModels"
@@ -39,7 +40,10 @@ input_test_loader = load_mnist(datafolder, 1, train=False, download=False, shuff
 distModelname = "distModel%s" % modelname
 distModel = load_model(distanceModel, model_folder, distModelname, trainstep, distPretrained)
 if torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
     distModel.cuda()
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
 
 
 # writers and criterion
@@ -57,31 +61,32 @@ for i in range(N_test_samples):
     input_test, label_test = next(iterInputTestLoader)
     input_test_batch = input_test.expand(train_batch_size, -1, -1, -1)
     distances = torch.zeros(0)
+    labels_train_all = torch.zeros(0).long()
     iterSearchTrainLoader = iter(search_train_loader)
     if torch.cuda.is_available():
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
         input_test_batch = input_test_batch.cuda()
+        distances = distances.cuda()
+        labels_train_all = labels_train_all.cuda()
     for j in range(N_train_batches):
         input_train_search, label_train_search = next(iterSearchTrainLoader)
         if torch.cuda.is_available():
             input_train_search = input_train_search.cuda()
 
         distancesTmp = distModel.forward(input_test_batch, input_train_search)
-        distances = torch.cat((distances,distancesTmp),0)
+        distances = torch.cat((distances, distancesTmp),0)
+        labels_train_all = torch.cat((labels_train_all, label_train_search),0)
 
-    _, sortedIndices = distances.sort()
-    nnLabel = label_train_search[sortedIndices[0]]
+    sortedDistances, sortedIndices = distances.sort(0)
+    nnLabel = labels_train_all[sortedIndices[0]]
     total += 1
-    correct += (nnLabel == label_test[0]).sum()
-    top10NNs[i] = sortedIndices[:10]
+    correct += (nnLabel == label_test[0]).sum().item()
+    top10NNs[i] = sortedIndices[:10].squeeze()
     if i%100==0:
-    # print("True Label: %i,  nnLabel: %i" % (label_test[0], nnLabel))
         print("Total: %i,   correct: %i" % (total, correct))
 
 
 print('Accuracy of the network on the 10000 test images: %f %%' % (
     100 * correct / total))
-
 
 
 print('Finished Full Search')
@@ -90,4 +95,5 @@ print(log_name)
 # save distances?
 # writer.close()
 
-torch.save(top10NNs, "distances/%stop10NNs"%log_name)
+distancesFilename = "distances/%stop10NNs"%log_name
+torch.save(top10NNs, distancesFilename)
